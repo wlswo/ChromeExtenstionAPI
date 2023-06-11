3723300975e75bf7422221ef39689bc0be27a797
wlswo <103496262+JaeJinByun@users.noreply.github.com>
2023-06-11 23:39:34 +0900

    [INIT] Initial development environment setting

---
+++ src/main/java/com/ChromeEX/Chrome/Extenstion/API/ChromeExtenstionApiApplication.java
@@ -0,0 +1,13 @@
+package com.ChromeEX.Chrome.Extenstion.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChromeExtenstionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChromeExtenstionApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ChromeEX/Chrome/Extenstion/API/ChromeExtenstionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ChromeEX.Chrome.Extenstion.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChromeExtenstionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
