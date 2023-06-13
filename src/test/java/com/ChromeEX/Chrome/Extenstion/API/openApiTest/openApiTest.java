ef58f99439caa666dc59f44e90f607eff7b19b47
wlswo <103496262+JaeJinByun@users.noreply.github.com>
2023-06-13 21:50:35 +0900

    [TEST] Upbit OpenAPI TEST to get Balance

---
+++ src/test/java/com/ChromeEX/Chrome/Extenstion/API/openApiTest/openApiTest.java
@@ -0,0 +1,56 @@
+package com.ChromeEX.Chrome.Extenstion.API.openApiTest;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.PropertySource;
+
+import java.io.IOException;
+import java.util.UUID;
+
+
+@SpringBootTest
+@PropertySource("classpath:application.yml")
+public class openApiTest {
+
+    @Value("${upbit-open-api.access-key}")
+    private String accessKey;
+
+    @Value("${upbit-open-api.secret-key}")
+    private String secretKey;
+
+    private final String serverUrl = "https://api.upbit.com";
+
+    @Test
+    void 업비트_가격_조회() {
+        Algorithm algorithm = Algorithm.HMAC256(secretKey);
+        String jwtToken = JWT.create()
+                .withClaim("access_key", accessKey)
+                .withClaim("nonce", UUID.randomUUID().toString())
+                .sign(algorithm);
+
+        String authenticationToken = "Bearer " + jwtToken;
+
+        try {
+            HttpClient client = HttpClientBuilder.create().build();
+            HttpGet request = new HttpGet(serverUrl + "/v1/accounts");
+            request.setHeader("Content-Type", "application/json");
+            request.addHeader("Authorization", authenticationToken);
+
+            HttpResponse response = client.execute(request);
+            HttpEntity entity = response.getEntity();
+
+            System.out.println(EntityUtils.toString(entity, "UTF-8"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
