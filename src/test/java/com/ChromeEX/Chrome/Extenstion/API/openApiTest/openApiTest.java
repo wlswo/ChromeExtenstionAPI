d4df7ec6d798e2cbc2b194f26a3c23313a53341e
wlswo <103496262+JaeJinByun@users.noreply.github.com>
2023-06-19 23:06:01 +0900

    [TEST/SUCCESS] upbit-api 잔고 가져오기 테스트

--- src/test/java/com/ChromeEX/Chrome/Extenstion/API/openApiTest/openApiTest.java
+++ src/test/java/com/ChromeEX/Chrome/Extenstion/API/openApiTest/openApiTest.java
@@ -2,6 +2,7 @@ package com.ChromeEX.Chrome.Extenstion.API.openApiTest;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import lombok.Getter;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
@@ -11,33 +12,43 @@ import org.apache.http.util.EntityUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+import org.springframework.core.env.Environment;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.UUID;
 
-
 @SpringBootTest
-@PropertySource("classpath:adapter-http-property-apikeys.yml")
-public class openApiTest {
+@Configuration
+@PropertySources({
+        @PropertySource(name = "app", value = "classpath:application.properties", encoding = "UTF-8")
+})
+@Getter
+public class openApiTest implements EnvironmentAware {
 
-    @Value("${upbit-open-api.access-key}")
+    @Override
+    public void setEnvironment(Environment env) {
+    }
+
+    @Value("#{'${access-key}'.split(';')}")
     private List<String> accessKey;
 
-    @Value("${upbit-open-api.secret-key}")
+    @Value("#{'${secret-key}'.split(';')}")
     private List<String> secretKey;
 
     private final String serverUrl = "https://api.upbit.com";
+
     @Test
     void 자산_조회() {
-        for (int i = 0; i < accessKey.size(); i++) {
+        for(int i=0; i<accessKey.size(); i++) {
             API_호출(accessKey.get(i),secretKey.get(i));
-            System.out.println(accessKey.size());
         }
     }
-    
-    
+
     void API_호출(String accessK, String secretK) {
         Algorithm algorithm = Algorithm.HMAC256(secretK);
         String jwtToken = JWT.create()
@@ -62,3 +73,4 @@ public class openApiTest {
         }
     }
 }
+
