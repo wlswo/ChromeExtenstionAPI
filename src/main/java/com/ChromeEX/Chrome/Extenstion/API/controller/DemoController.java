912aedbea54447f72c0126019daecb762bf10804
wlswo <103496262+JaeJinByun@users.noreply.github.com>
2023-06-23 21:36:19 +0900

    [REFACTORING] URL 변경, Import 최적화

--- src/main/java/com/ChromeEX/Chrome/Extenstion/API/controller/DemoController.java
+++ src/main/java/com/ChromeEX/Chrome/Extenstion/API/controller/DemoController.java
@@ -1,17 +1,18 @@
 package com.ChromeEX.Chrome.Extenstion.API.controller;
 
+import com.ChromeEX.Chrome.Extenstion.API.service.Implement.demoServiceImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.configurationprocessor.json.JSONException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.ChromeEX.Chrome.Extenstion.API.service.Implement.demoServiceImpl;
 
 @RequiredArgsConstructor
 @RestController
 public class DemoController {
     private final demoServiceImpl demoService;
 
-    @GetMapping("/")
-    public String getBalance() {
+    @GetMapping("/bit")
+    public String getBalance() throws JSONException {
         return demoService.getBalance();
     }
 
